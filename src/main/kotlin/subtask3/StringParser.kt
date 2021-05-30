8ff5d71a5fdf919be8a14616e465e52ac7936427
Anton-Pankratov <anton.pankratov7526@gmail.com>
2021-05-31 00:20:26 +0600

    complete: subtask3

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -1,5 +1,14 @@
 package subtask3
 
+/**
+ * [substringsOrder] - list of found substrings with order for future parsing in [result].
+ * Represent by [MutableList] of [Pair], where 'first' is a order number in [result] and 'second'
+ * is [IntRange] of [substring] in "inputString" of [getResult].
+ *
+ * [openings] - list of found opening brackets in string, where 'first' of [Pair] is bracket char and
+ * 'second' is index of "inputString"
+ */
+
 class StringParser {
     private val result = mutableListOf<String>()
     private val substringsOrder = mutableListOf<Pair<Int, IntRange>>()
@@ -8,46 +17,32 @@ class StringParser {
     fun getResult(inputString: String): Array<String> {
 
         inputString.forEachIndexed { index, char ->
-            when (char) {
-                '(', '[', '<' -> {
-                    openings.add(char to index)
-                }
-                ')' -> {
-                    openings.apply {
-                        val element = lastOrNull { it.first == ')' }
-                        val orderNumber: Int
-                        if (element != null) {
-                            orderNumber = this.lastIndexOf(element)
-                            substringsOrder.add(orderNumber to (element.second - 1)..index)
-                        }
-
-                    }
-                }
-                ']' -> {
-                    openings.apply {
-                        val element = lastOrNull { it.first == ']' }
-                        val orderNumber: Int
-                        if (element != null) {
-                            orderNumber = this.lastIndexOf(element)
-                            substringsOrder.add(orderNumber to (element.second - 1)..index)
-                        }
-
-                    }
-                }
-                '>' -> {
-                    openings.apply {
-                        val element = lastOrNull { it.first == '>' }
-                        val orderNumber: Int
-                        if (element != null) {
-                            orderNumber = this.lastIndexOf(element)
-                            substringsOrder.add(orderNumber to (element.second - 1)..index)
-                        }
-                    }
+            (char to index).apply {
+                when (char) {
+                    '(', '[', '<' -> openings.add(this)
+                    ')' -> '('.formInterBracketsRange(this)
+                    ']' -> '['.formInterBracketsRange(this)
+                    '>' -> '<'.formInterBracketsRange(this)
                 }
             }
         }
 
-        return arrayOf()
+        substringsOrder.sortedBy { it.first }.forEach {
+            result.add(inputString.substring(it.second))
+        }
+
+        return result.toTypedArray()
     }
 
+    private fun Char.formInterBracketsRange(pair: Pair<Char, Int>) {
+        openings.apply {
+            val element = lastOrNull { it.first == this@formInterBracketsRange }
+            val orderNumber: Int
+            if (element != null) {
+                orderNumber = this.lastIndexOf(element)
+                substringsOrder.add(orderNumber to((element.second + 1) until pair.second))
+                set(indexOf(element), '*' to -1)
+            }
+        }
+    }
 }
